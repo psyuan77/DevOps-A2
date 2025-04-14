697dc073df016f27aadde3c64f7413f8b323475d
psyuan77@hotmail.com <156147051+psyuan77@users.noreply.github.com>
2025-04-14 00:08:37 -0700

    change configure to match my repo name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-app/'  // Replace with your repository name
+  base: '/DevOps-A2/'  // Replace with your repository name
 })
